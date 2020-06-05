89836697e523dbcf35e936d3e4dac74cc7c571fd
JP Swinski <jp.swinski@nasa.gov>
2020-06-05 11:41:19 -0400

    reorganized py scripts

--- plugins/icesat2/pyclient.py
+++ plugins/icesat2/pyclient.py
@@ -1,97 +1,96 @@
-# python
-
-import sys
-import json
-import requests
-import struct
-
+#
+# Parameters
+#
 server_url = 'http://127.0.0.1:9081'
+api = "h5"
+max_bytes = 0x80000
 
-###############################################################################
-# APIs
-###############################################################################
+filename = "/data/ATLAS/ATL03_20200304065203_10470605_003_01.h5"
+dataset = "gt2l/heights/dist_ph_along"
+datatag = 0
 
-#
-#  ECHO
-#
-def echo (parm):
-    url = '%s/echo' % server_url
-    return requests.post(url, data=parm)
-
-#
-#  SOURCE
-#
-def source (api, parm):
-    url = '%s/source/%s' % (server_url, api)
-    return requests.post(url, data=parm)
+rqst_dict = {
+        "filename": filename,
+        "dataset": dataset,
+        "id": datatag
+    }
 
 #
-#  ENGINE
+# Imports
+#
+import sys
+import requests
+import json
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from ipywidgets import interact
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+
+#
+# SlideRule API
 #
 def engine (api, parm):
     url = '%s/engine/%s' % (server_url, api)
     return requests.post(url, data=parm, stream=True)
 
-
-###############################################################################
-# TESTS
-###############################################################################
-
 #
-#  TEST ECHO
+# Default Matplotlib Plot of Dataframe Column
 #
-def test_echo ():
-    d = echo('{ "hello" : "world" }').json()
-    if d["hello"] == "world":
-        print("Passed echo test")
+def dfplot(df, col, cond_col="", cond_val=0):
+    if cond_col != "":
+        c = df[df[cond_col] == cond_val]
+        s = c[pd.isnull(c[col]) == False][col]
     else:
-        print("Failed echo test")
+        s = df[pd.isnull(df[col]) == False][col]
+    s.plot(figsize=(15,5))
+    plt.title(col)
+    plt.show()
 
 #
-#  TEST TIME
+# Sum of Dataframe Column
 #
-def test_time ():
-    rqst_dict = {
-        "time": "NOW",
-        "input": "NOW",
-        "output": "GPS"
-    }
-
-    d = source("time", json.dumps(rqst_dict)).json()
-
-    now = d["time"] - (d["time"] % 1000) # gmt is in resolution of seconds, not milliseconds
-    rqst_dict["time"] = d["time"]
-    rqst_dict["input"] = "GPS"
-    rqst_dict["output"] = "GMT"
-
-    d = source("time", json.dumps(rqst_dict)).json()
-
-    rqst_dict["time"] = d["time"]
-    rqst_dict["input"] = "GMT"
-    rqst_dict["output"] = "GPS"
-
-    d = source("time", json.dumps(rqst_dict)).json()
-
-    again = d["time"]
-
-    if now == again:
-        print("Passed time test")
+def dfsum(df, col, cond_col="", cond_val=0):
+    if cond_col != "":
+        total = df[df[cond_col] == cond_val][col].sum()
     else:
-        print("Failed time test")
+        total = df[col].sum()
+    return total
 
 #
-#  TEST H5
+# Matplotlib Histogram of Value Counts in Dataframe Column
 #
-def test_h5 ():
-    rqst_dict = {
-        "filename": "/data/ATLAS/ATL03_20200304065203_10470605_003_01.h5",
-        "dataset": "ancillary_data/atlas_sdp_gps_epoch",
-        "id": 0
-    }
+def dfplotvc(df, col, cond_col="", cond_val=0):
+    if cond_col != "":
+        vc = df[df[cond_col] == cond_val][col].value_counts()
+    else:
+        vc = df[col].value_counts()
+    plt.clf()
+    plt.figure(figsize=(15,8))
+    vc.plot(kind='bar')
+    plt.show()
 
-    d = engine("h5", json.dumps(rqst_dict))
-    for line in d.iter_content(0x1000):
-        print(binascii.hexlify(line))
+#
+# Interactive Bokeh Plot of Dataframe Column
+# ... note that x axis is a recreated list of item number
+# ... and does not reflect original index in dataframe
+#
+def dfbokeh(df, col, cond_col="", cond_val=0):
+    p = figure(title=col, plot_height=300, plot_width=600)
+    if cond_col != "":
+        c = df[df[cond_col] == cond_val]
+        s = c[pd.isnull(c[col]) == False][col]
+    else:
+        s = df[pd.isnull(df[col]) == False][col]
+    y = s.values
+    x = [i for i in range(len(y))]
+    r = p.line(x, y, color="#2222aa", line_width=3)
+    show(p, notebook_handle=True)
+
 
 ###############################################################################
 # MAIN
@@ -100,11 +99,35 @@ def test_h5 ():
 if __name__ == '__main__':
 
     # Override server URL from command line
-
     if len(sys.argv) > 1:
         server_url = sys.argv[1]
 
-    # Tests
-    test_echo()
-    test_time()
-    test_h5()
+    # Make API Request
+    d = engine(api, json.dumps(rqst_dict))
+
+    # Read Response
+    response_bytes = 0
+    responses = []
+    for line in d.iter_content(0x10000):
+        if line:
+            response_bytes += len(line)
+            responses.append(line)
+            if response_bytes >= max_bytes:
+                break
+        else:
+            print("Keep Alive")
+
+    # Build DataFrame
+    raw = b''.join(responses)
+    size = int(len(raw) / 4)
+    values = np.frombuffer(raw, dtype=np.float32, count=size)
+    df = pd.DataFrame(data=values, index=[i for i in range(size)], columns=[dataset])
+
+    # Display Results
+    dfbokeh(df, dataset)
+
+
+#
+#   Notes:
+#       data = np.hstack(responses) # efficiently concatenates list of numpy arrays
+#
---
+++ plugins/icesat2/pytest.py
@@ -0,0 +1,119 @@
+# python
+
+import sys
+import json
+import requests
+import struct
+import binascii
+
+server_url = 'http://127.0.0.1:9081'
+
+###############################################################################
+# APIs
+###############################################################################
+
+#
+#  ECHO
+#
+def echo (parm):
+    url = '%s/echo' % server_url
+    return requests.post(url, data=parm)
+
+#
+#  SOURCE
+#
+def source (api, parm):
+    url = '%s/source/%s' % (server_url, api)
+    return requests.post(url, data=parm)
+
+#
+#  ENGINE
+#
+def engine (api, parm):
+    url = '%s/engine/%s' % (server_url, api)
+    return requests.post(url, data=parm, stream=True)
+
+
+###############################################################################
+# TESTS
+###############################################################################
+
+#
+#  TEST ECHO
+#
+def test_echo ():
+    d = echo('{ "hello" : "world" }').json()
+    if d["hello"] == "world":
+        print("Passed echo test")
+    else:
+        print("Failed echo test")
+
+#
+#  TEST TIME
+#
+def test_time ():
+    rqst_dict = {
+        "time": "NOW",
+        "input": "NOW",
+        "output": "GPS"
+    }
+
+    d = source("time", json.dumps(rqst_dict)).json()
+
+    now = d["time"] - (d["time"] % 1000) # gmt is in resolution of seconds, not milliseconds
+    rqst_dict["time"] = d["time"]
+    rqst_dict["input"] = "GPS"
+    rqst_dict["output"] = "GMT"
+
+    d = source("time", json.dumps(rqst_dict)).json()
+
+    rqst_dict["time"] = d["time"]
+    rqst_dict["input"] = "GMT"
+    rqst_dict["output"] = "GPS"
+
+    d = source("time", json.dumps(rqst_dict)).json()
+
+    again = d["time"]
+
+    if now == again:
+        print("Passed time test")
+    else:
+        print("Failed time test")
+
+#
+#  TEST H5
+#
+def test_h5 ():
+    rqst_dict = {
+        "filename": "/data/ATLAS/ATL03_20200304065203_10470605_003_01.h5",
+        "dataset": "ancillary_data/atlas_sdp_gps_epoch",
+        "id": 0
+    }
+
+    epoch_offset = (0.0,)
+
+    d = engine("h5", json.dumps(rqst_dict))
+    for line in d.iter_content(0x1000):
+        if epoch_offset[0] == 0.0:
+            epoch_offset = struct.unpack('d', line)
+
+    if(epoch_offset[0] == 1198800018.0):
+        print("Passed h5 test")
+    else:
+        print("Failed h5 test")
+
+###############################################################################
+# MAIN
+###############################################################################
+
+if __name__ == '__main__':
+
+    # Override server URL from command line
+
+    if len(sys.argv) > 1:
+        server_url = sys.argv[1]
+
+    # Tests
+    test_echo()
+    test_time()
+    test_h5()
