da8c6ce3829adce3b20d37a2908ec9e1102c4685
JP Swinski <jp.swinski@nasa.gov>
2020-06-11 19:01:15 -0400

    successful self test run inside docker container

--- scripts/test/dictionary.lua
+++ scripts/test/dictionary.lua
@@ -4,8 +4,8 @@ local rd = runner.rootdir(arg[0])
 -- Dictionary Unit Test --
 
 runner.command("NEW UT_DICTIONARY ut_dictionary")
-runner.command(string.format('ut_dictionary::ADD_WORD_SET small %s../../scripts/test/alphabet_words.txt', rd))
-runner.command(string.format('ut_dictionary::ADD_WORD_SET large %s../../scripts/test/english_words.txt', rd))
+runner.command(string.format('ut_dictionary::ADD_WORD_SET small %s/alphabet_words.txt', rd))
+runner.command(string.format('ut_dictionary::ADD_WORD_SET large %s/english_words.txt', rd))
 runner.command("ut_dictionary::FUNCTIONAL_TEST small")
 runner.command("ut_dictionary::FUNCTIONAL_TEST large")
 runner.command("ut_dictionary::ITERATOR_TEST small")
