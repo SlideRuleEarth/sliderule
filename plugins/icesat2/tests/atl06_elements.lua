0b6de4dac4f88fd1f96f06959671d6fb89e2fd60
JP Swinski <jp.swinski@nasa.gov>
2020-07-13 14:47:10 -0400

    removed DIST element of atl06 elements test

--- plugins/icesat2/tests/atl06_elements.lua
+++ plugins/icesat2/tests/atl06_elements.lua
@@ -34,8 +34,6 @@ if extentrec then
     runner.check(extentrec:getvalue("SEG_ID[1]") == 555764, extentrec:getvalue("SEG_ID[1]"))
     runner.check(runner.cmpfloat(extentrec:getvalue("GPS[0]"), 1267339942.127844, 0.0001), extentrec:getvalue("GPS[0]"))
     runner.check(runner.cmpfloat(extentrec:getvalue("GPS[1]"), 1267339942.472445, 0.0001), extentrec:getvalue("GPS[1]"))
-    runner.check(runner.cmpfloat(extentrec:getvalue("DIST[0]"), 11132813.466047, 0.0001), extentrec:getvalue("DIST[0]"))
-    runner.check(runner.cmpfloat(extentrec:getvalue("DIST[1]"), 11132813.466047, 0.0001), extentrec:getvalue("DIST[1]"))
     runner.check(extentrec:getvalue("COUNT[0]") == 136, extentrec:getvalue("COUNT[0]"))
     runner.check(extentrec:getvalue("COUNT[1]") == 539, extentrec:getvalue("COUNT[1]"))
 end
