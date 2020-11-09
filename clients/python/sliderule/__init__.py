3887f0c0248fa254e3d65388f4bb4187cffe1d03
JP Swinski <jp.swinski@nasa.gov>
2020-11-09 11:21:45 -0500

    removed python client code from this repo, moved to sliderule-python

--- clients/python/sliderule/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-sliderule
-=========
-
-A C++/Lua framework for on-demand science data processing
-"""
-from sliderule.sliderule import *
--- plugins/icesat2/clients/python/icesat2/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-icesat2
-=========
-
-A C++/Lua framework for on-demand science data processing
-"""
-from icesat2.icesat2 import *
