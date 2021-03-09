c8472b151baeb3da220f535e39cb0bac153638c6
JP Swinski <jp.swinski@nasa.gov>
2021-03-09 10:51:28 -0500

    python bindings return python list

--- targets/h5coro-python/h5coro-python.cpp
+++ targets/h5coro-python/h5coro-python.cpp
@@ -38,16 +38,89 @@ namespace py = pybind11;
 
 struct H5LiteFile
 {
+    /*--------------------------------------------------------------------
+     * constructor
+     *--------------------------------------------------------------------*/
     H5LiteFile(const std::string &_url): url(_url) { }
 
-    int read(const std::string &datasetname, int valtype, long col, long startrow, long numrows) 
+    /*--------------------------------------------------------------------
+     * read
+     *--------------------------------------------------------------------*/
+    py::list* read(const std::string &datasetname, long col, long startrow, long numrows) 
     { 
-        if(numrows < 0) numrows = H5Coro::ALL_ROWS; // workaround for binding to default argument value
-        H5Coro::info_t info = H5Coro::read(url.c_str(), datasetname.c_str(), (RecordObject::valType_t)valtype, col, startrow, numrows, &context);
+        // workaround for binding to default argument value
+        if(numrows < 0) numrows = H5Coro::ALL_ROWS;
+
+        // perform read of dataset
+        H5Coro::info_t info = H5Coro::read(url.c_str(), datasetname.c_str(), RecordObject::DYNAMIC, col, startrow, numrows, &context);
+
+        // build dataset array
+        py::list* dataset = new py::list;
+        if(info.datatype == RecordObject::REAL)
+        {
+            if(info.typesize == sizeof(double))
+            {
+                double* data_ptr = (double*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+            else if(info.typesize == sizeof(float))
+            {
+                float* data_ptr = (float*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+        }
+        else if(info.datatype == RecordObject::INTEGER)
+        {
+            if(info.typesize == sizeof(long))
+            {
+                long* data_ptr = (long*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+            else if(info.typesize == sizeof(int))
+            {
+                int* data_ptr = (int*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+            else if(info.typesize == sizeof(short))
+            {
+                short* data_ptr = (short*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+            else if(info.typesize == sizeof(unsigned char))
+            {
+                unsigned char* data_ptr = (unsigned char*)info.data;
+                for(int i = 0; i < info.elements; i++)
+                {
+                    dataset->append(data_ptr[i]);
+                }
+            }
+        }
+        
+        // clean up data
         if(info.data) delete [] info.data;
-        return (int)info.datasize;
+
+        // return list
+        return dataset;
     }
 
+    /*--------------------------------------------------------------------
+     * data
+     *--------------------------------------------------------------------*/
     std::string url;
     H5Coro::context_t context;
 };
@@ -61,15 +134,14 @@ PYBIND11_MODULE(h5coro, m)
     m.doc() = "H5Coro module for read-only access to *.h5 files";
 
     py::class_<H5LiteFile>(m, "file")
+        
         .def(py::init<const std::string &>())
+        
         .def("read", &H5LiteFile::read, "reads dataset from file", 
             py::arg("dataset"), 
-            py::arg("type"), 
             py::arg("col") = 0, 
             py::arg("startrow") = 0, 
             py::arg("numrows") = -1);
 
     m.attr("all") = (long)-1;
-    m.attr("float") = (long)RecordObject::REAL;
-    m.attr("int") = (long)RecordObject::INTEGER;
 }
