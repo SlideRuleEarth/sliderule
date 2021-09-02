7f08205f81c232e0c125fc9c2a33e061018ccf6f
JP Swinski <jp.swinski@nasa.gov>
2021-09-02 14:34:16 -0400

    fixed memory freeing error... PktStats in CfsInterface already deleted

--- packages/legacy/CfsInterface.cpp
+++ packages/legacy/CfsInterface.cpp
@@ -1,31 +1,31 @@
 /*
  * Copyright (c) 2021, University of Washington
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the University of Washington nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ *
+ * 3. Neither the name of the University of Washington nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS
- * “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+ * “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -71,6 +71,10 @@ int CfsInterface::PktStats::rec_elem = sizeof(CfsInterface::PktStats::rec_def) /
 CfsInterface::PktStats::PktStats(CommandProcessor* cmd_proc, const char* stat_name): StatisticRecord<pktStats_t>(cmd_proc, stat_name, rec_type)
 {
     cmdProc->registerObject(stat_name, this);
+}
+
+CfsInterface::PktStats::~PktStats(void)
+{
 }
 
 /******************************************************************************
@@ -199,13 +203,10 @@ CfsInterface::~CfsInterface(void)
     if(tlmQ) delete tlmQ;
     if(cmdQ) delete cmdQ;
 
-    for(int i = 0; i < CCSDS_NUM_APIDS + 1; i++)
-    {
-        if(apidStats[i] != NULL)
-        {
-            cmdProc->deleteObject(apidStats[i]->getName());
-        }
-    }
+    // PktStats (apidStats[]) do not need to be explicitly
+    // deleted here; that is handled as a part of the
+    // command processor clean up, since they are
+    // registered as CommandableObjects
 }
 
 /*----------------------------------------------------------------------------
