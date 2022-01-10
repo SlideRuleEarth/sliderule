976df7bb7af57c6045847588e2de20c5da4bc8fb
JP Swinski <jp.swinski@nasa.gov>
2022-01-10 14:50:23 -0500

    prometheus endpoint returns openmetric formatted response

--- scripts/endpoints/prometheus.lua
+++ scripts/endpoints/prometheus.lua
@@ -6,11 +6,16 @@
 -- OUTPUT:      OpentMetrics Text Format (used by prometheus)
 --
 
-local json = require("json")
-local prettyprint = require("prettyprint")
+local rsp_str = ""
 
 local metrics = sys.metric()
 
-prettyprint.display(metrics)
+for k, v in pairs(metrics) do
+    local metric_name = k:gsub("%.","_")
+    local metric_type = v["type"]
+    local metric_value = (metric_type == "counter") and string.format("%d", tonumber(v["value"])) or tostring(v["value"])
+    rsp_str = rsp_str .. string.format("\n# TYPE %s %s\n", metric_name, metric_type)
+    rsp_str = rsp_str .. string.format("%s %s\n", metric_name, metric_value)
+end
 
-return json.encode(metrics)
+return rsp_str
