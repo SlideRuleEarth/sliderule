b5857d940705d33d540c16635948b56ca6d5061e
JP Swinski <jp.swinski@nasa.gov>
2022-01-13 11:41:31 -0500

    fixed formatting issue in prometheus endpoint when counter not an integer

--- scripts/endpoints/prometheus.lua
+++ scripts/endpoints/prometheus.lua
@@ -13,7 +13,7 @@ local metrics = sys.metric()
 for k, v in pairs(metrics) do
     local metric_name = k:gsub("[%.-:]","_")
     local metric_type = v["type"]
-    local metric_value = (metric_type == "counter") and string.format("%d", tonumber(v["value"])) or tostring(v["value"])
+    local metric_value = tostring(v["value"])
     rsp_str = rsp_str .. string.format("\n# TYPE %s %s\n", metric_name, metric_type)
     rsp_str = rsp_str .. string.format("%s %s\n", metric_name, metric_value)
 end
