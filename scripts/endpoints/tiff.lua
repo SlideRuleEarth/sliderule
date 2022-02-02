9d759cb41931c3a8427918989f86a48be8fa69b1
JP Swinski <jp.swinski@nasa.gov>
2022-02-02 09:45:18 -0500

    cleaned up tiff endpoint

--- scripts/endpoints/tiff.lua
+++ scripts/endpoints/tiff.lua
@@ -13,17 +13,21 @@ local json = require("json")
 local base64 = require("base64")
 local parm = json.decode(arg[1])
 
+function ascii_display(raster)
+    rows, cols = raster:dim()
+    print("DIM: ", rows, cols)
+    for r = 0,rows,1 do
+        for c = 0,cols,1 do
+            io.write(raster:pixel(r, c) and "#" or " ")
+        end
+        io.write("\n")
+    end
+end
+
 local tiff = base64.decode(parm["raster"])
 local size = parm["imagelength"]
 
 raster = geotiff.scan(tiff, size)
-rows, cols = raster:dim()
-print("DIM: ", rows, cols)
-for r = 0,rows,1 do
-    for c = 0,cols,1 do
-        io.write(raster:pixel(r, c) and "#" or " ")
-    end
-    io.write("\n")
-end
+ascii_display(raster)
 
-return json.encode({complete=true})
+return json.encode({input=parm})
