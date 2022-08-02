214fabdc40ea9e5343f7eccd42ee3e234694275b
Eric Lidwa <eric.lidwa@nasa.gov>
2022-08-02 13:59:17 -0400

    memtest script fix, failed lua test cause vagriand to return error even none were found

--- scripts/systests/memtest.sh
+++ scripts/systests/memtest.sh
@@ -6,10 +6,15 @@ testscript="scripts/selftests/test_runner.lua"
 valgrind_suppressions="scripts/systests/memtest.supp"
 valgrind_options="--leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --error-exitcode=1 --suppressions=${valgrind_suppressions}"
 
-printf "\nStarting memory test...\n\n"
+printf "\nStarting memory test, output redirected to %s\n\n" $logfile
 
-#valgrind ${valgrind_options} ${progname} ${testscript} >${logfile} 2>&1
-valgrind ${valgrind_options} ${progname} ${testscript} 
+valgrind ${valgrind_options} ${progname} ${testscript} >${logfile} 2>&1
+
+cat $logfile
+
+#grep for error status, do not trust --error-exitcode=1 since failed lua test will cause 
+#valgrind to return an error and not what we expect (0 since valgrind did not find any errors)
+grep -w "ERROR SUMMARY: 0 errors" ${logfile}
 ret=$?
 
 if [ $ret == 0 ]; 
@@ -19,6 +24,4 @@ else
     printf "\nMemory test FAILED\n"
 fi
 
-# return valgrind exit status
-# --error-exitcode=1 will cause valgrind to return 1 on any errors or 0 when none
 exit $ret
