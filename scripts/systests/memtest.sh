71d1f6effe5b27ddf71dcfcb8e96854e7fb8cda8
Eric Lidwa <eric.lidwa@nasa.gov>
2022-08-02 14:32:13 -0400

    better way of loging valgrind output

--- scripts/systests/memtest.sh
+++ scripts/systests/memtest.sh
@@ -4,18 +4,21 @@ logfile=memtest.log
 progname=sliderule
 testscript="scripts/selftests/test_runner.lua"
 valgrind_suppressions="scripts/systests/memtest.supp"
-valgrind_options="--leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --error-exitcode=1 --suppressions=${valgrind_suppressions}"
+valgrind_options="--leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --log-file=${logfile} --suppressions=${valgrind_suppressions}"
 
 printf "\nStarting memory test, output redirected to %s\n\n" $logfile
 
-valgrind ${valgrind_options} ${progname} ${testscript} >${logfile} 2>&1
-
-cat $logfile
+#valgrind ${valgrind_options} ${progname} ${testscript} >${logfile} 2>&1
+valgrind ${valgrind_options} ${progname} ${testscript} 
 
 #grep for error status, do not trust --error-exitcode=1 since failed lua test will cause 
 #valgrind to return an error and not what we expect (0 since valgrind did not find any errors)
-grep -w "ERROR SUMMARY: 0 errors" ${logfile}
+grep -q -w "ERROR SUMMARY: 0 errors" ${logfile}
 ret=$?
+
+
+printf "\n\n********* valgrind output *********\n\n"
+cat $logfile
 
 if [ $ret == 0 ]; 
 then
