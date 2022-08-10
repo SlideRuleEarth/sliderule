9454d4cde301a939a3a602607d9a7aaceb4ac644
JP Swinski <jp.swinski@nasa.gov>
2022-08-10 12:33:01 -0400

    updated http faults test to reflect that http client needs to reconnect after a fault

--- scripts/selftests/http_faults.lua
+++ scripts/selftests/http_faults.lua
@@ -7,23 +7,27 @@ local json = require("json")
 endpoint = core.endpoint()
 server   = core.httpd(9081):attach(endpoint, "/source"):untilup()
 
-client = core.http("127.0.0.1", 9081)
 
 -- Unit Test --
 
+client = core.http("127.0.0.1", 9081)
 rsps, code, status = client:request("GET", "/", "{}")
 runner.check(status == nil, "failed to report error on unattached endpoint")
+client:destroy()
 
+client = core.http("127.0.0.1", 9081)
 rsps, code, status = client:request("RAW", "/", "\r\n\r\n")
 runner.check(status == nil, "failed to report error on empty http request")
+client:destroy()
 
+client = core.http("127.0.0.1", 9081)
 rsps, code, status = client:request("GET", "/source/health", "{}")
 runner.check(status == true, "failed to remain healthy after invalid requests")
+client:destroy()
 
 -- Clean Up --
 
 server:destroy()
-client:destroy()
 
 -- Report Results --
 
