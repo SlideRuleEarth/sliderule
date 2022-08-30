459e8af342fcdb07fb66a5b598442d5c4f3b8846
Eric Lidwa <eric.lidwa@nasa.gov>
2022-08-30 12:14:54 -0400

    added  lua test for raster pkg

--- scripts/selftests/geojson_raster.lua
+++ scripts/selftests/geojson_raster.lua
@@ -25,13 +25,17 @@ local params = {data = vectorfile, length = len, cellsize = cellsize}
 
 print('\n------------------\nTest01: Create\n------------------')
 local robj = raster.file(params)
+runner.check(robj ~= nil)
+
+
+print('\n------------------\nTest02: dim\n------------------')
 local rows, cols  = robj:dim()
 print("rows: ", rows, "cols: ", cols)
 runner.check(rows == 3)
 runner.check(cols == 6)
 
 
-print('\n------------------\nTest02: bbox\n------------------')
+print('\n------------------\nTest03: bbox\n------------------')
 local lon_min, lat_min, lon_max, lat_max = robj:bbox()
 print("lon_min: ", lon_min, "lat_min: ", lat_min, "\nlon_max: ", lon_max, "lat_max: ", lat_max)
 runner.check(lon_min ~= 0)
@@ -39,12 +43,12 @@ runner.check(lat_min ~= 0)
 runner.check(lon_max ~= 0)
 runner.check(lon_max ~= 0)
 
-print('\n------------------\nTest03: cellsize\n------------------')
+print('\n------------------\nTest04: cellsize\n------------------')
 local _cellsize = robj:cell()
 print("cellsize: ", _cellsize)
 runner.check(_cellsize == cellsize)
 
-print('\n------------------\nTest04: pixel\n------------------')
+print('\n------------------\nTest05: pixel\n------------------')
 local pixelIn = robj:pixel(rows-1, cols-3)
 print("pixel in raster check: ", pixelIn)
 runner.check(pixelIn == true)
