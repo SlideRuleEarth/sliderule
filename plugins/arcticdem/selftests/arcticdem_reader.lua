7c9e06d1324426cab7fb57d70877dd4a3fd495ba
Eric Lidwa <eric.lidwa@nasa.gov>
2022-09-09 12:51:48 -0400

    testing 3 cache modes for arctic raster

--- plugins/arcticdem/selftests/arcticdem_reader.lua
+++ plugins/arcticdem/selftests/arcticdem_reader.lua
@@ -12,7 +12,7 @@ local arcticdem_local = core.getbyname(asset_name)
 
 -- Unit Test --
 
-print('\n------------------\nTest01: Create ArcticDemRaster \n------------------')
+print('\n------------------\nMilion Points All In Cache\n------------------')
 
 for ctype = 0, 2
 do
@@ -23,17 +23,65 @@ do
     local arcdem = arcticdem.raster(ctype)
     runner.check(arcdem ~= nil)
 
-    for i = 1, 10
+    local starttime = time.latch();
+    for i = 1, 1000000
     do
         el, status = arcdem:subset(lon, lat)
-        print(i, status, el)
-        lat = lat + 0.0001
-        lon = lon + 0.0001
+        if status ~= true then
+            print(i, status, el)
+        end
+        if (i % 2 == 0) then
+            lon = lon + 0.0001
+         -- lat = lat + 0.0001
+        else
+            lon = lon - 0.0001
+         -- lat = lat - 0.0001
+        end
     end
+    local stoptime = time.latch();
+    local dtime = stoptime-starttime
+    print('ExecTIme:',dtime*1000, '\n')
 
     arcdem = nil
 end
 
+
+print('\n------------------\n10 Points All Cache Misses\n------------------')
+
+for ctype = 0, 2
+do
+    local lat =  82.86
+    local lon = -74.60
+    local el, status
+
+    local arcdem = arcticdem.raster(ctype)
+    runner.check(arcdem ~= nil)
+
+    local starttime = time.latch();
+    -- for i = 1, 1000000
+    for i = 1, 10
+    do
+        el, status = arcdem:subset(lon, lat)
+        if status ~= true then
+            print(i, status, el)
+        end
+        if (i % 2 == 0) then
+            lon = lon + 0.2
+            lat = lat + 0.2
+        else
+            lon = lon - 0.2
+            lat = lat - 0.2
+        end
+    end
+    local stoptime = time.latch();
+    local dtime = stoptime-starttime
+    print('ExecTIme:',dtime*1000, '\n')
+
+    arcdem = nil
+end
+
+
+os.exit()
 
 
 print('\n------------------\nTest01: ArcticDEM Reader \n------------------')
