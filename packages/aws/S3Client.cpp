1c5ad25ec79e0d18b56ec24ec7bb9f4e3ab11b54
JP Swinski <jp.swinski@nasa.gov>
2022-09-21 18:58:09 +0000

    removed accidental code

--- packages/aws/S3Client.cpp
+++ packages/aws/S3Client.cpp
@@ -38,6 +38,7 @@
 #include "core.h"
 
 #include <curl/curl.h>
+#include <openssl/hmac.h>
 
 /******************************************************************************
  * PRIVATE IMPLEMENTATION
@@ -99,35 +100,6 @@ S3Client::~impl (void)
     curl_easy_cleanup(curl);
 }
 
-
-
-#include <string.h>
-#include <openssl/hmac.h>
-
-int main()
-{
-    // The key to hash
-    char key[] = "012345678";
-
-    // The data that we're going to hash using HMAC
-    char data[] = "hello world";
-
-    unsigned char* digest;
-
-    // Using sha1 hash engine here.
-    // You may use other hash engines. e.g EVP_md5(), EVP_sha224, EVP_sha512, etc
-    digest = HMAC(EVP_sha1(), key, strlen(key), (unsigned char*)data, strlen(data), NULL, NULL);
-
-    // Be careful of the length of string with the choosen hash engine. SHA1 produces a 20-byte hash value which rendered as 40 characters.
-    // Change the length accordingly with your choosen hash engine
-    char mdString[20];
-    for(int i = 0; i < 20; i++)
-         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
-
-    printf("HMAC digest: %s\n", mdString);
-
-    return 0;
-}
 /*----------------------------------------------------------------------------
  * read
  *----------------------------------------------------------------------------*/
