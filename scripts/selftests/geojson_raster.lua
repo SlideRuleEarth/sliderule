f6f2d403915d4b0f2971663d1af1523f6e6a4e84
Eric Lidwa <eric.lidwa@nasa.gov>
2022-12-15 19:00:15 +0000

    fixed geojson_raster script

--- scripts/selftests/geojson_raster.lua
+++ scripts/selftests/geojson_raster.lua
@@ -24,20 +24,20 @@ local cellsize = 0.1
 local params = {data = vectorfile, length = len, cellsize = cellsize}
 
 print('\n------------------\nTest01: Create\n------------------')
-local robj = raster.file(params)
+local robj = geo.geojson(params)
 runner.check(robj ~= nil)
 
 
 print('\n------------------\nTest02: dim\n------------------')
 local rows, cols  = robj:dim()
-print("rows: ", rows, "cols: ", cols)
+print(string.format("dimensions: rows: %d, cols: %d", rows, cols))
 runner.check(rows == 3)
 runner.check(cols == 6)
 
 
 print('\n------------------\nTest03: bbox\n------------------')
 local lon_min, lat_min, lon_max, lat_max = robj:bbox()
-print("lon_min: ", lon_min, "lat_min: ", lat_min, "\nlon_max: ", lon_max, "lat_max: ", lat_max)
+print(string.format("lon_min: %.2f, lat_min: %.2f, lon_max: %.2f, lan_max: %.2f", lon_min, lat_min, lon_max, lat_max))
 runner.check(lon_min ~= 0)
 runner.check(lat_min ~= 0)
 runner.check(lon_max ~= 0)
@@ -45,16 +45,16 @@ runner.check(lon_max ~= 0)
 
 print('\n------------------\nTest04: cellsize\n------------------')
 local _cellsize = robj:cell()
-print("cellsize: ", _cellsize)
+print(string.format("cellsize: %f", _cellsize))
 runner.check(_cellsize == cellsize)
 
 print('\n------------------\nTest05: pixel\n------------------')
 local pixelIn = robj:pixel(rows-1, cols-3)
-print("pixel in raster check: ", pixelIn)
+print("pixel in raster check:", pixelIn)
 runner.check(pixelIn == true)
 
 pixelIn = robj:pixel(rows-1, cols-1)
-print("pixel out of raster check: ", pixelIn)
+print("pixel out of raster check:", pixelIn)
 runner.check(pixelIn == false)
 
 
