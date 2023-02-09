2bb240f65db4ad34eb88e116fdc858f71e7fc4fe
JP Swinski <jp.swinski@nasa.gov>
2023-02-09 01:26:48 +0000

    fixes for endpoings using geo parms

--- plugins/icesat2/endpoints/atl06.lua
+++ plugins/icesat2/endpoints/atl06.lua
@@ -67,7 +67,7 @@ if parms[geo.PARMS] then
     local elevation_rec_type = parms["compact"] and "atl06rec-compact.elevation" or "atl06rec.elevation"
     sampler_disp = core.dispatcher(rspq, 1) -- 1 thread required because GeoRaster is not thread safe
     for key,settings in pairs(parms[geo.PARMS]) do
-        local robj = geo.raster(geo.parm(settings))
+        local robj = geo.raster(geo.parms(settings))
         if robj then
             local sampler = geo.sampler(robj, key, rspq, elevation_rec_type, "extent_id", "lon", "lat")
             if sampler then
