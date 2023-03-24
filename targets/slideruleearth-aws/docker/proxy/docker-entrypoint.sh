b8e8afa82f94fee559bec72bee45b07b2cb936e4
JP Swinski <jp.swinski@nasa.gov>
2023-03-24 18:54:19 +0000

    moved auth configuration into its own file

--- targets/slideruleearth-aws/docker/proxy/docker-entrypoint.sh
+++ targets/slideruleearth-aws/docker/proxy/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst "\$CLIENT_ID \$CLIENT_SECRET \$DOMAIN \$REDIRECT_URI_SCHEME" < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst "\$CLIENT_ID \$CLIENT_SECRET \$DOMAIN \$REDIRECT_URI_SCHEME" < /usr/local/openresty/nginx/conf/auth.conf.template > /usr/local/openresty/nginx/conf/auth.conf
 openresty -g "daemon off;"
