89c4a0a12220d85ed72da7586e3a85c9cf59f4e0
JP Swinski <jp.swinski@nasa.gov>
2023-03-24 16:57:08 +0000

    handle redirect uri scheme as an environment variable to support running locally

--- targets/slideruleearth-aws/docker/proxy/docker-entrypoint.sh
+++ targets/slideruleearth-aws/docker/proxy/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst "\$CLIENT_ID \$CLIENT_SECRET \$DOMAIN" < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst "\$CLIENT_ID \$CLIENT_SECRET \$DOMAIN \$REDIRECT_URI_SCHEME" < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 openresty -g "daemon off;"
