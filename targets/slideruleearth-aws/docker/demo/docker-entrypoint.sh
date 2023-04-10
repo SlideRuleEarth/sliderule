a52f15d4adf2bace7fa2d992adb054660f71897c
JP Swinski <jp.swinski@nasa.gov>
2023-04-10 15:27:06 +0000

    removed voila demo from this repo

--- targets/slideruleearth-aws/docker/demo/docker-entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-conda run --no-capture-output -n sliderule voila --theme=dark --no-browser --Voila.ip=0.0.0.0 --Voila.base_url='/demo' --MappingKernelManager.cull_interval=60 --MappingKernelManager.cull_idle_timeout=120 /python/voila_demo.ipynb
