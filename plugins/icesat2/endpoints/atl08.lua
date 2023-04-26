5c17ea692871875969c1117f004903043427fc44
JP Swinski <jp.swinski@nasa.gov>
2023-04-26 15:23:32 +0000

    made common resource processing extension

--- plugins/icesat2/endpoints/atl08.lua
+++ plugins/icesat2/endpoints/atl08.lua
@@ -1,136 +1,32 @@
 --
 -- ENDPOINT:    /source/atl08
 --
--- PURPOSE:     generate customized atl08 data product
---
--- INPUT:       rqst
---              {
---                  "resource":     "<url of hdf5 file or object>"
---                  "parms":        {<table of parameters>}
---              }
---
---              rspq - output queue to stream results
---
--- OUTPUT:      atl08rec (ATL0* algorithm results)
---
--- NOTES:       1. The rqst is provided by arg[1] which is a json object provided by caller
---              2. The rspq is the system provided output queue name string
---              3. The output is a raw binary blob containing serialized 'atl08rec' RecordObjects
---
 
-local json = require("json")
+local json          = require("json")
+local georesource   = require("georesource")
 
--- Create User Status --
-local userlog = msg.publish(rspq)
+local rqst          = json.decode(arg[1])
+local resource      = rqst["resource"]
+local parms         = rqst["parms"]
 
--- Request Parameters --
-local rqst = json.decode(arg[1])
-local resource = rqst["resource"]
-local parms = rqst["parms"]
-local atl03_asset = parms["asset"] or rqst["atl03-asset"] or "icesat2"
-parms["asset"] = atl03_asset -- backward compatibility layer
-local timeout = parms["node-timeout"] or parms["timeout"] or netsvc.NODE_TIMEOUT
-local shard = rqst["shard"] or 0 -- key space
+local args = {
+    shard           = rqst["shard"] or 0, -- key space
+    default_asset   = "icesat2",
+    source_q        = resource .. "." .. rspq,
+    result_q        = parms[geo.PARMS] and "result." .. resource .. "." .. rspq or rspq,
+    source_rec      = "atl08rec",
+    result_rec      = "atl08rec",
+    result_batch    = "atl08rec.vegetation",
+    index_field     = "extent_id",
+    lon_field       = "lon",
+    lat_field       = "lat"
+}
 
--- Initialize Timeouts --
-local duration = 0
-local interval = 10 < timeout and 10 or timeout -- seconds
+local rqst_parms    = icesat2.parms(parms)
+local algo          = icesat2.atl08(args.result_q, rqst_parms)
+local proc          = georesource.initialize(resource, parms, algo, args)
 
--- Get Asset --
-local asset = core.getbyname(atl03_asset)
-if not asset then
-    userlog:sendlog(core.INFO, string.format("invalid asset specified: %s", atl03_asset))
-    do return end
+if proc then
+    local reader    = icesat2.atl03(proc.asset, resource, args.source_q, rqst_parms, true)
+    local status    = georesource.waiton(resource, parms, algo, reader, proc.algo_disp, proc.sampler_disp, proc.userlog, true)
 end
-
--- Create Record Queue --
-local recq = resource .. "." .. rspq
-
--- ATL08 Dispatcher --
-local atl08_disp = core.dispatcher(recq)
-
--- Request Parameters */
-local rqst_parms = icesat2.parms(parms)
-
--- Exception Forwarding --
-local except_pub = core.publish(rspq)
-atl08_disp:attach(except_pub, "exceptrec") -- exception records
-atl08_disp:attach(except_pub, "extrec") -- ancillary records
-
--- ATL08 Dispatch Algorithm --
-local atl08_algo = icesat2.atl08(rspq, rqst_parms)
-atl08_disp:attach(atl08_algo, "atl03rec")
-
--- Raster Sampler --
-local sampler_disp = nil
-if parms[geo.PARMS] then
-    local atl08_rec_type = "atl08rec"
-    local elevation_rec_type = "atl08rec.vegetation"
-    sampler_disp = core.dispatcher(rspq, 1) -- 1 thread required because GeoRaster is not thread safe
-    for key,settings in pairs(parms[geo.PARMS]) do
-        local robj = geo.raster(geo.parms(settings):keyspace(shard))
-        if robj then
-            local sampler = geo.sampler(robj, key, rspq, elevation_rec_type, "extent_id", "lon", "lat")
-            if sampler then
-                sampler_disp:attach(sampler, atl08_rec_type)
-            else
-                userlog:sendlog(core.CRITICAL, string.format("request <%s> failed to create sampler %s for %s", rspq, key, resource))
-            end
-        else
-            userlog:sendlog(core.CRITICAL, string.format("request <%s> failed to create raster %s for %s", rspq, key, resource))
-        end
-    end
-    sampler_disp:run()
-end
-
--- Run ATL08 Dispatcher --
-atl08_disp:run()
-
--- Post Initial Status Progress --
-userlog:sendlog(core.INFO, string.format("request <%s> atl08 processing initiated on %s ...", rspq, resource))
-
--- ATL03 Reader --
-local atl03_reader = icesat2.atl03(asset, resource, recq, rqst_parms, true)
-
--- Wait Until Reader Completion --
-while (userlog:numsubs() > 0) and not atl03_reader:waiton(interval * 1000) do
-    duration = duration + interval
-    -- Check for Timeout --
-    if timeout >= 0 and duration >= timeout then
-        userlog:sendlog(core.ERROR, string.format("request <%s> for %s timed-out after %d seconds", rspq, resource, duration))
-        do return end
-    end
-    userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to read %s (after %d seconds)", rspq, resource, duration))
-end
-
--- Resource Processing Complete
-local atl03_stats = atl03_reader:stats(false)
-userlog:sendlog(core.INFO, string.format("request <%s> processing of %s complete (%d/%d/%d/%d)", rspq, resource, atl03_stats.read, atl03_stats.sent, atl03_stats.filtered, atl03_stats.dropped))
-
--- Wait Until ATL08 Dispatch Completion --
-while (userlog:numsubs() > 0) and not atl08_disp:waiton(interval * 1000) do
-    duration = duration + interval
-    -- Check for Timeout --
-    if timeout >= 0 and duration >= timeout then
-        userlog:sendlog(core.ERROR, string.format("request <%s> timed-out after %d seconds", rspq, duration))
-        do return end
-    end
-    userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to process ATL03 records (after %d seconds)", rspq, duration))
-end
-
--- Wait Until Sampler Dispatch Completion --
-if sampler_disp then
-    sampler_disp:aot() -- aborts on next timeout
-    while (userlog:numsubs() > 0) and not sampler_disp:waiton(interval * 1000) do
-        duration = duration + interval
-        -- Check for Timeout --
-        if timeout >= 0 and duration >= timeout then
-            userlog:sendlog(core.ERROR, string.format("request <%s> timed-out after %d seconds", rspq, duration))
-            do return end
-        end
-        userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to sample ATL06 records (after %d seconds)", rspq, duration))
-    end
-end
-
--- Request Processing Complete
-userlog:sendlog(core.INFO, string.format("request <%s> processing complete", rspq))
---
+++ scripts/extensions/georesource.lua
@@ -0,0 +1,148 @@
+--
+-- EXTENSION:   georesource.lua
+--
+-- PURPOSE:     generate customized goelocated data products
+--
+-- INPUT:       rqst
+--              {
+--                  "resource":     "<url of hdf5 file or object>"
+--                  "parms":        {<table of parameters>}
+--              }
+--
+--              rspq - output queue to stream results
+--
+-- OUTPUT:      algorithm results
+--
+-- NOTES:       1. The rqst is provided by arg[1] which is a json object provided by caller
+--              2. The rspq is the system provided output queue name string
+--              3. The output is a raw binary blob containing serialized RecordObjects
+--
+
+local json = require("json")
+
+--
+-- Initialize Processing of Resource
+--
+local function initialize(resource, parms, algo, args)
+
+    -- User Status --
+    local userlog = msg.publish(rspq)
+
+    -- Get Asset --
+    local asset_name = parms["asset"] or default_asset
+    local asset = core.getbyname(asset_name)
+    if not asset then
+        userlog:sendlog(core.INFO, string.format("invalid asset specified: %s", asset_name))
+        do return nil end
+    end
+
+    -- Raster Sampler --
+    local sampler_disp = nil
+    if parms[geo.PARMS] then
+        rsps_bridge = core.bridge(args.result_q, rspq)
+        sampler_disp = core.dispatcher(args.result_q, 1) -- 1 thread required because GeoRaster is not thread safe
+        for key,settings in pairs(parms[geo.PARMS]) do
+            local robj = geo.raster(geo.parms(settings):keyspace(args.shard))
+            if robj then
+                local sampler = geo.sampler(robj, key, rspq, args.result_batch, args.index_field, args.lon_field, args.lat_field)
+                if sampler then
+                    sampler_disp:attach(sampler, args.result_rec)
+                else
+                    userlog:sendlog(core.CRITICAL, string.format("request <%s> failed to create sampler %s for %s", rspq, key, resource))
+                end
+            else
+                userlog:sendlog(core.CRITICAL, string.format("request <%s> failed to create raster %s for %s", rspq, key, resource))
+            end
+        end
+        sampler_disp:run()
+    end
+
+    -- Dispatcher --
+    local algo_disp = core.dispatcher(args.source_q)
+
+    -- Attach Exception and Ancillary Record Forwarding --
+    local except_pub = core.publish(rspq)
+    algo_disp:attach(except_pub, "exceptrec") -- exception records
+    algo_disp:attach(except_pub, "extrec") -- ancillary records
+
+    -- Attach Algorithm --
+    algo_disp:attach(algo, args.source_rec)
+
+    -- Run Dispatcher --
+    algo_disp:run()
+
+    -- Post Initial Status Progress --
+    userlog:sendlog(core.INFO, string.format("request <%s> processing initialized on %s ...", rspq, resource))
+
+    -- Return Needed Objects to Continue Processing Request --
+    return {asset=asset, algo_disp=algo_disp, sample_disp=sampler_disp, userlog=userlog}
+end
+
+--
+-- Wait On Processing of Resource
+--
+local function waiton(resource, parms, algo, reader, algo_disp, sampler_disp, userlog, with_stats)
+
+    -- Initialize Timeouts --
+    local timeout = parms["node-timeout"] or parms["timeout"] or netsvc.NODE_TIMEOUT
+    local duration = 0
+    local interval = 10 < timeout and 10 or timeout -- seconds
+
+    -- Wait Until Reader Completion --
+    while (userlog:numsubs() > 0) and not reader:waiton(interval * 1000) do
+        duration = duration + interval
+        -- Check for Timeout --
+        if timeout >= 0 and duration >= timeout then
+            userlog:sendlog(core.ERROR, string.format("request <%s> for %s timed-out after %d seconds", rspq, resource, duration))
+            do return false end
+        end
+        userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to read %s (after %d seconds)", rspq, resource, duration))
+    end
+
+    -- Resource Processing Complete --
+    if with_stats then
+        local reader_stats = reader:stats(false)
+        userlog:sendlog(core.INFO, string.format("request <%s> processing of %s complete (%d/%d/%d)", rspq, resource, reader_stats.read, reader_stats.filtered, reader_stats.dropped))
+    end
+
+    -- Wait Until ATL06 Dispatch Completion --
+    while (userlog:numsubs() > 0) and not algo_disp:waiton(interval * 1000) do
+        duration = duration + interval
+        -- Check for Timeout --
+        if timeout >= 0 and duration >= timeout then
+            userlog:sendlog(core.ERROR, string.format("request <%s> timed-out after %d seconds", rspq, duration))
+            do return false end
+        end
+        userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to process source records (after %d seconds)", rspq, duration))
+    end
+
+    -- Wait Until Sampler Dispatch Completion --
+    if sampler_disp then
+        sampler_disp:aot() -- aborts on next timeout
+        while (userlog:numsubs() > 0) and not sampler_disp:waiton(interval * 1000) do
+            duration = duration + interval
+            -- Check for Timeout --
+            if timeout >= 0 and duration >= timeout then
+                userlog:sendlog(core.ERROR, string.format("request <%s> timed-out after %d seconds", rspq, duration))
+                do return false end
+            end
+            userlog:sendlog(core.INFO, string.format("request <%s> ... continuing to sample records (after %d seconds)", rspq, duration))
+        end
+    end
+
+    -- Request Processing Complete --
+    if with_stats then
+        local algo_stats = algo:stats(false)
+        userlog:sendlog(core.INFO, string.format("request <%s> processing complete (%d/%d/%d/%d)", rspq, algo_stats.read, algo_stats.filtered, algo_stats.posted, algo_stats.dropped))
+    end
+
+    -- Return Success --
+    return true
+end
+
+local package = {
+    initialize = initialize,
+    waiton = waiton
+}
+
+return package
