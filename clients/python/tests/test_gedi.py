bc6ec22f81aa2707650d163cd922444a2f93cb7a
JP Swinski <jp.swinski@nasa.gov>
2023-06-02 19:52:23 +0000

    added gedi l4b pytest

--- clients/python/tests/test_gedi.py
+++ clients/python/tests/test_gedi.py
@@ -108,3 +108,32 @@ class TestL4A:
         granules = earthdata.cmr(short_name="GEDI_L4A_AGB_Density_V2_1_2056", polygon=region["poly"])
         assert len(granules) >= 130
         assert 'GEDI04_A_' in granules[0]
+
+@pytest.mark.network
+class TestL4B:
+    def test_gedi(self, domain, asset, organization, desired_nodes):
+        gedi.init(domain, organization=organization, desired_nodes=desired_nodes, bypass_dns=True)
+        resource = "ATL03_20220105023009_02111406_005_01.h5"
+        region = sliderule.toregion(os.path.join(TESTDIR, "data/grandmesa.geojson"))
+        parms = {
+            "poly": region['poly'],
+            "srt": icesat2.SRT_LAND,
+            "len": 100,
+            "res": 100,
+            "track": 1,
+            "pass_invalid": True,
+            "atl08_class": ["atl08_ground", "atl08_canopy", "atl08_top_of_canopy"],
+            "phoreal": {"binsize": 1.0, "geoloc": "center", "use_abs_h": False, "send_waveform": False},
+            "samples": {"gedi": {"asset": "gedil4b"}}
+        }
+        gdf = icesat2.atl08p(parms, asset=asset, resources=[resource], keep_id=True)
+        exp_keys = ['snowcover', 'h_max_canopy', 'canopy_h_metrics', 'solar_elevation',
+                    'h_canopy', 'spot', 'segment_id', 'rgt', 'gnd_ph_count', 'h_te_median',
+                    'cycle', 'ph_count', 'h_mean_canopy', 'distance', 'landcover',
+                    'veg_ph_count', 'h_min_canopy', 'canopy_openness', 'gt', 'extent_id',
+                    'geometry', 'gedi.flags', 'gedi.time', 'gedi.value', 'gedi.file_id']
+        for key in exp_keys:
+            assert key in gdf.keys()
+        assert abs(gdf.describe()["canopy_openness"]["max"] - 10.390829) < 0.001
+        df = gdf[gdf["gedi.value"] > -9999.0]
+        assert abs(sum(df["gedi.value"]) - 31148.828197479248) < 0.001
