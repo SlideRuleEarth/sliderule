828eaba62d7f66ca10eb35893b103ad14c533d26
JP Swinski <jp.swinski@nasa.gov>
2023-09-20 14:43:27 +0000

    improving log archiving and access

--- targets/slideruleearth-aws/terraform/cluster/export_logs.sh
+++ targets/slideruleearth-aws/terraform/cluster/export_logs.sh
@@ -4,7 +4,8 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/bin:$PATH
 clustername=`cat /clustername.txt` 
 logfile=$clustername$(date +'_%Y_%m_%d_log.txt')
-logcli query -o raw --limit 500000000 '{job="sliderule"}' > ${logfile} || exit 1
+fromdate=$(date -d "yesterday" '+%Y-%m-%d')T00:00:00Z
+logcli query -o jsonl --limit 1000000 --batch 5000 --from=${fromdate} '{job="sliderule",level=~"debug|info|warning|error|critical"}' > ${logfile} || exit 1
 gzip -9 ${logfile}
 aws s3 cp ${logfile}.gz s3://sliderule/logs/ || exit 1
 rm ${logfile}.gz
