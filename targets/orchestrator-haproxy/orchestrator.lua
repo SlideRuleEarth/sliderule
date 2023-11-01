1b718e3b15946f4119b161c991c663581d7770f5
JP Swinski <jp.swinski@nasa.gov>
2023-11-01 15:12:44 +0000

    fix in orchestrator for gauge metrics; updated sliderule node application metrics grafana page

--- targets/orchestrator-haproxy/orchestrator.lua
+++ targets/orchestrator-haproxy/orchestrator.lua
@@ -484,7 +484,7 @@ local function api_metric(applet)
         applet:set_status(200)
     elseif metric_type == 1 then
         StatData.gaugeAppMetrics[metric_name] = metric_value
-        StatData.gaugeAppMetrics[metric_name .. "_sum"] = (StatData.countAppMetrics[metric_name .. "_sum"] or 0.0) + metric_value
+        StatData.gaugeAppMetrics[metric_name .. "_sum"] = (StatData.gaugeAppMetrics[metric_name .. "_sum"] or 0.0) + metric_value
         StatData.countAppMetrics[metric_name .. "_count"] = (StatData.countAppMetrics[metric_name .. "_count"] or 0) + 1
         applet:set_status(200)
     else
