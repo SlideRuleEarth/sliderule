fa20fcb8b9aa0468116e236357176f33a13dfbec
JP Swinski <jp.swinski@nasa.gov>
2023-11-13 19:47:38 +0000

    updated gedi pytest to reflect new gdal sampling code

--- clients/python/tests/test_gedi.py
+++ clients/python/tests/test_gedi.py
@@ -79,7 +79,7 @@ class TestL3:
         gdf = icesat2.atl06p(parms, resources=[resource])
         assert init
         assert gdf.describe()["gedi.time"]["std"] == 0.0
-        assert abs(gdf.describe()["gedi.value"]["mean"] - 3142.8526604140434) < 0.001
+        assert abs(gdf.describe()["gedi.value"]["mean"] - 3143.5934365441703) < 0.001
         assert gdf.describe()["gedi.file_id"]["max"] == 0.0
         assert gdf.describe()["gedi.flags"]["max"] == 0.0
 
@@ -136,4 +136,4 @@ class TestL4B:
             assert key in gdf.keys()
         assert abs(gdf.describe()["canopy_openness"]["max"] - 10.390829086303711) < 0.001
         df = gdf[gdf["gedi.value"] > -9999.0]
-        assert abs(sum(df["gedi.value"]) - 42551.55461392179) < 0.001
+        assert abs(sum(df["gedi.value"]) - 42767.289459228516) < 0.001
