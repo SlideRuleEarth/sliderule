46ac4959deac0306b83a31ac72c5fb3b0e264284
ksharonin <ksharonin@berkeley.edu>
2024-01-13 08:13:31 +0000

    enable debug mode w gdb flag, target new nc file

--- Makefile
+++ Makefile
@@ -21,6 +21,10 @@ all: ## build code
 config: prep ## configure make for release version of sliderule
 	cd $(BUILD); cmake -DCMAKE_BUILD_TYPE=Release $(CFG) $(ROOT)
 
+config-debug: prep 
+	cd $(BUILD); cmake -DCMAKE_BUILD_TYPE=Debug $(CFG) $(ROOT)
+	$(eval CXXFLAGS += -g)
+	
 config-library: prep ## configure make for shared library libsliderule.so
 	cd $(BUILD); cmake -DCMAKE_BUILD_TYPE=Release -DSHARED_LIBRARY=ON $(CFG) $(ROOT)
 
--- scripts/selftests/h5coro.lua
+++ scripts/selftests/h5coro.lua
@@ -1,27 +1,3 @@
-local runner = require("test_executive")
-local console = require("console") -- non trivial printing
-local td = runner.rootdir(arg[0]) -- set dir
-
--- Setup --
-
-console.monitor:config(core.LOG, core.DEBUG) -- looking at log mssg infos, include all and debug: DEBUG, INFO, WARNING, ERROR, CRITICAL --
--- monitor set; does it have ability to log this? need to attach inspection
--- mul monitors per time
-sys.setlvl(core.LOG, core.DEBUG) -- global level set to debug, print to screen
-
-asset = core.asset("local", "nil", "file", td, "empty.index")
--- asset == driving file, config user/access, set up do-nothing for local
-
--- Unit Test --
-
-print('\n------------------\nTest: Read Dataset\n------------------')
-
-dataq = "dataq" -- stream based: data comes in, then consumed/produced
--- produce: produce stream
--- consume: subscribe to stream
--- one subscribing, get anything coming on from point on
-rsps2 = msg.subscribe(dataq) -- responses posted to dataq
-
 -- TODO:
 -- replace zip h5 with nc
 -- read diff variables + attrs
@@ -30,19 +6,100 @@ rsps2 = msg.subscribe(dataq) -- responses posted to dataq
 -- verbose printing/writing to file
 -- expect: 3 weeks~ understanding code and parsing
 
-f2 = h5.dataset(core.READER, asset, "h5ex_d_gzip.h5", "/DS1", 0, true, core.INTEGER, 2, 0, core.ALL_ROWS)
+--[[
+    from h5datasetdevice.cpp:
+    create(<role>, 
+            <asset>, 
+            <resource>,
+            <dataset name>, 
+            [<id>], --> ?
+            [<raw>], --> ?
+            [<datatype>], 
+            [col], 
+            [startrow], 
+            [numrows])
+    args:
+    - reader <role>
+    - asset 
+    - file path <resource>
+    - variable <datasetname>
+    - ? <id>
+    - ? <raw>
+    - dtype
+    - col
+    - start row
+    - num rows
+]]
+
+local runner = require("test_executive")
+local console = require("console") -- non trivial printing
+local td = runner.rootdir(arg[0]) -- set dir
+
+-- Setup --
+
+-- monitor set; does it have ability to log this? need to attach inspection
+-- mul monitors per time
+-- looking at log mssg infos, include all and debug: DEBUG, INFO, WARNING, ERROR, CRITICAL
+-- asset == driving file, config user/access, set up do-nothing for local
+-- global level set to debug, print to screen
+
+console.monitor:config(core.LOG, core.DEBUG) 
+sys.setlvl(core.LOG, core.DEBUG)
+
+asset = core.asset("local", "nil", "file", td, "empty.index")
+
+-- Unit Test --
+
+print('\n------------------\nTest: Read Dataset\n------------------')
+
+-- produce: produce stream
+-- consume: subscribe to stream
+-- one subscribing, get anything coming on from point on
+
+dataq = "dataq" -- stream based: data comes in, then consumed/produced
+rsps2 = msg.subscribe(dataq) -- responses posted to dataq
+
+-- Arguments & Call --
+
+local resource_path = "../OR_ABI-L2-FDCC-M3_G17_s20182390052191_e20182390054564_c20182390055159.nc"
+local dataset_name = "/Area"
+local id = 0
+local raw = true
+local dtype = core.INTEGER
+local col = 0
+local startrow = 0
+local numrows = core.ALL_ROWS
+
+f2 = h5.dataset(core.READER, 
+                asset, 
+                resource_path_path, 
+                dataset_name, 
+                id, 
+                raw, 
+                dtype, 
+                col, 
+                startrow, 
+                numrows)
+
 -- reader for h5, h5 packages exposes dataset --> reader: needs asset, name, path, variable, row/col
--- still nothing...
 r2 = core.reader(f2, dataq) -- actually reads
 -- pass dataset into the stream (post into stream)
 
 vals = rsps2:recvstring(3000) -- read out from listner
+
+print("\n vals recieved with type: ".. type(vals))
+
 e1, e2, e3, e4 = string.unpack('iiii', vals) -- unpack as ints and check
 
-runner.check(-2 == e1, "failed dataset read")
-runner.check( 0 == e2, "failed dataset read")
-runner.check( 2 == e3, "failed dataset read")
-runner.check( 4 == e4, "failed dataset read")
+print("\n e1 val: " .. e1)
+print("e2 val: " ..e2)
+print("e3 val: " ..e3)
+print("e4 val: " ..e4)
+
+-- runner.check(-2 == e1, "failed dataset read")
+-- runner.check( 0 == e2, "failed dataset read")
+-- runner.check( 2 == e3, "failed dataset read")
+-- runner.check( 4 == e4, "failed dataset read") 
 
 rsps2:destroy()
 r2:destroy()
