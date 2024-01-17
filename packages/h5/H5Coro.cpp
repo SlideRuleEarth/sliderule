237bf0533bb8799101a11ffacb9c365d35811e43
ksharonin <ksharonin@berkeley.edu>
2024-01-17 10:50:35 +0000

    begin enum feature, note fill value bug

--- packages/h5/H5Coro.cpp
+++ packages/h5/H5Coro.cpp
@@ -55,6 +55,31 @@
 #endif
 
 /******************************************************************************
+ * KAT-CUSTOM ENUM MAPPING
+ ******************************************************************************/
+typedef enum H5T_cset_t {
+    H5T_CSET_ERROR       = -1, /**< error                           */
+    H5T_CSET_ASCII       = 0,  /**< US ASCII                        */
+    H5T_CSET_UTF8        = 1,  /**< UTF-8 Unicode encoding		     */
+    H5T_CSET_RESERVED_2  = 2,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_3  = 3,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_4  = 4,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_5  = 5,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_6  = 6,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_7  = 7,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_8  = 8,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_9  = 9,  /**< reserved for later use		     */
+    H5T_CSET_RESERVED_10 = 10, /**< reserved for later use		     */
+    H5T_CSET_RESERVED_11 = 11, /**< reserved for later use		     */
+    H5T_CSET_RESERVED_12 = 12, /**< reserved for later use		     */
+    H5T_CSET_RESERVED_13 = 13, /**< reserved for later use		     */
+    H5T_CSET_RESERVED_14 = 14, /**< reserved for later use		     */
+    H5T_CSET_RESERVED_15 = 15  /**< reserved for later use		     */
+} H5T_cset_t;
+
+#define H5T_NCSET H5T_CSET_RESERVED_2 /*Number of character sets actually defined  */
+
+/******************************************************************************
  * MACROS
  ******************************************************************************/
 
@@ -2871,8 +2896,9 @@ int H5FileBuffer::readAttributeMsg (uint64_t pos, uint8_t hdr_flags, int dlvl, u
         readByteArray(attr_name, name_size, &pos);
     }
     if (version == 3) {
+        // TODO: integrate encoding functionality 
+        // H5T_cset_t name_encoding;
         pos += 8;
-        // UTF-8 encoding
         readByteArray(attr_name, name_size, &pos);
     }
 
