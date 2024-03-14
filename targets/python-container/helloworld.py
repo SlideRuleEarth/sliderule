445aa3e764e691a0fbd2b22b2848d04808ff6cdc
JP Swinski <jp.swinski@nasa.gov>
2024-03-14 13:36:51 +0000

    working cre endpoint

--- targets/python-container/helloworld.py
+++ targets/python-container/helloworld.py
@@ -9,11 +9,10 @@ output_directory = sliderule_interface.get_output_directory()
 
 # write results to output file
 result = {"Hello": "World"}
-output_filename = os.path.join(output_directory, "result.json")
-with open(output_filename, "w") as file:
+output_filename = "result.json"
+with open(os.path.join(output_directory, output_filename), "w") as file:
+    print(f'Writing result to {output_filename}')
     json.dump(result, file)
 
 # set output file in sliderule interface
 sliderule_interface.set_output_files([output_filename])
-
-print("HELLO WORLD", output_directory)
