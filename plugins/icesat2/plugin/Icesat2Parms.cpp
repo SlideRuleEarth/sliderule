f8e97502db6aae75a1d1f7aaa32b4da64f122d14
JP Swinski <jp.swinski@nasa.gov>
2024-05-10 15:21:49 +0000

    optimized implementations of getting the spot number in icesat2 data; see #388

--- plugins/icesat2/plugin/Icesat2Parms.cpp
+++ plugins/icesat2/plugin/Icesat2Parms.cpp
@@ -102,7 +102,10 @@ int Icesat2Parms::luaCreate (lua_State* L)
         return returnLuaStatus(L, false);
     }
 }
-
+#if 0
+/*
+ * THESE FUNCTIONS HAVE BEEN IMPLEMENTED AS OPTIMIZED INLINE CALLS IN THE HEADER
+ */
 /*----------------------------------------------------------------------------
  * getSpotNumber
  *----------------------------------------------------------------------------*/
@@ -192,7 +195,7 @@ uint8_t Icesat2Parms::getGroundTrack (sc_orient_t sc_orient, track_t track, int 
 
     return 0;
 }
-
+#endif
 /*----------------------------------------------------------------------------
  * str2atl03cnf
  *----------------------------------------------------------------------------*/
