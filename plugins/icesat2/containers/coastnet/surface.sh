23d1f23fd405f98055392733d25319451028656e
JP Swinski <jp.swinski@nasa.gov>
2024-05-17 16:27:07 +0000

    simplified bathy and surface scripts

--- plugins/icesat2/containers/coastnet/surface.sh
+++ plugins/icesat2/containers/coastnet/surface.sh
@@ -1,18 +1,2 @@
 #!/usr/bin/bash
-
-# Bash strict mode
-set -euo pipefail
-IFS=$'\n\t'
-
-# Use debug build unless overridden from cmdline
-: ${build:=debug}
-
-echo build = ${build}
-
-find $1 | \
-    parallel --verbose --lb --jobs=15 \
-        "/coastnet/build/${build}/classify_coastnet \
-        --class=41 \
-        --aspect-ratio=4 \
-        --network-filename=$2 \
-        < {} > $3"
+/coastnet/build/debug/classify_coastnet --class=41 --aspect-ratio=4 --network-filename=$2 < $1 > $3
