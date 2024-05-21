fc51db93f5c85fa177c43ec519242c40202ca498
JP Swinski <jp.swinski@nasa.gov>
2024-05-21 11:40:45 +0000

    added verbose option to running coastnet surface classifier

--- plugins/icesat2/containers/coastnet/surface.sh
+++ plugins/icesat2/containers/coastnet/surface.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-/coastnet/build/debug/classify_coastnet --class=41 --aspect-ratio=4 --network-filename=/coastnet/models/coastnet-surface.pt < $3 > $4
+/coastnet/build/debug/classify_coastnet --verbose --class=41 --aspect-ratio=4 --network-filename=/coastnet/models/coastnet-surface.pt < $3 > $4
 source /venv/bin/activate
 python /usr/local/etc/combiner.py $3 $4
