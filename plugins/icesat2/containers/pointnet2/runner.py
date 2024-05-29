e26014cf372b00c3627bb72bb561e8a61179a1c8
JP Swinski <jp.swinski@nasa.gov>
2024-05-29 14:32:26 +0000

    small updates to pointnet2 runner

--- plugins/icesat2/containers/pointnet2/runner.py
+++ plugins/icesat2/containers/pointnet2/runner.py
@@ -112,13 +112,12 @@ else:
 # set configuration
 maxElev         = settings.get('maxElev', 10) 
 minElev         = settings.get('minElev', -50)
+demBuffer       = settings.get('demBuffer', 50)
 minSignalConf   = settings.get('minSignalConf', 3)
 minNWDI         = settings.get('minNWDI', 0.3)
 useNWDI         = settings.get('useNWDI', False)
-batch_size      = settings.get('batch_size', 1)
 gpu             = settings.get('gpu', "0")
 num_point       = settings.get('num_point', 8192)
-use_signal_conf = settings.get('use_signal_conf', True)
 num_votes       = settings.get('num_votes', 10)
 threshold       = settings.get('threshold', 0.5)
 
@@ -135,27 +134,24 @@ print(f'Preprocessing {input_csv}...')
 # read input csv and store into a dataframe
 beam_df = pd.read_csv(input_csv)
 
-# Add a class column
-beam_df['class'] = np.full((len(beam_df)), 3)
+# Add a pointnet specific class column
+beam_df['class'] = np.full((len(beam_df)), 3)                               # initialize everything to 3 (pointnet noise)
+beam_df.loc[beam_df.class_ph == 41, 'class'] = 5                            # change sea surface (41) to 5 (pointnet sea surface)
 
-# If beam_df.class_ph == 41 (sea surface), then set the class for beam_df at that same photon index to 5 (sea surface for PointNet)
-beam_df.loc[beam_df.class_ph == 41, 'class'] = 5
-
-# Get the subset of the sea_surface_df where class_ph is sea surface
-sea_surface_df = beam_df[beam_df['class'] == 5]
-# Take the average of the geoid corrected height of the sea surface photons.
-average_sea_surface_level = sea_surface_df['geoid_corr_h'].mean()
-# Only keep the photons that are lower than average sea surface level to decrease data volume.
+# Remove photons above sea surfaace
+sea_surface_df = beam_df[beam_df['class'] == 5]                             # get the subset of the sea_surface_df where class_ph is sea surface
+average_sea_surface_level = sea_surface_df['geoid_corr_h'].mean()           # take the average of the geoid corrected height of the sea surface photons
 beam_df = beam_df[beam_df['geoid_corr_h'] < average_sea_surface_level]
-# Keep photons where the max_signal_conf is greater than a minimum threshold.
+
+# Remove photons where the max_signal_conf doesn't meet minimum threshold
 beam_df = beam_df[beam_df['max_signal_conf'] >= minSignalConf]
 
-# Remove irrelevant photons (deeper than 50m, higher than 10m)
-beam_df = beam_df[(beam_df["geoid_corr_h"] > minElev) & (beam_df["geoid_corr_h"] < maxElev)]
-    
-beam_df = beam_df[(beam_df["dem_h"] - beam_df["geoid_corr_h"] < 50)]
+# Remove photons outside of height range
+beam_df = beam_df[(beam_df["geoid_corr_h"] > minElev) & \
+                  (beam_df["geoid_corr_h"] < maxElev)]                      # if geoid height is outside absolute range
+beam_df = beam_df[(beam_df["dem_h"] - beam_df["geoid_corr_h"] < demBuffer)] # if geoid height is outside relative range to DEM
 
-# Remove photons where the NDWI value is greater than a minimum threshold.
+# Remove photons where the NDWI value is greater than a minimum threshold
 if useNWDI:
     beam_df = beam_df[beam_df["ndwi"] >= minNWDI]
 
@@ -176,19 +172,14 @@ data_df['signal_conf_ph'] = data_df['signal_conf_ph'] - 2
 # Create the 2d numpy array used by pointnet
 data = data_df.to_numpy().astype(np.float64)
 
-################
-# RUN POINTNET2
-################
+###########################
+# HELPER FUNCTIONS & CLASS
+###########################
 
 def to_categorical(y, num_classes):
     """ 1-hot encodes a tensor """
     new_y = torch.eye(num_classes)[y.cpu().data.numpy(),]
     return new_y.to(y.device)
-
-def pc_denormalize(pc, pc_min, pc_max):
-    for i in range(pc.shape[1]):
-        pc[:, i] = (pc[:, i] + 1) / 2 * (pc_max[i] - pc_min[i]) + pc_min[i]
-    return pc
 
 def pc_normalize(pc):
     pc_min = np.empty(3, dtype=np.float64)
@@ -200,74 +191,67 @@ def pc_normalize(pc):
     return pc, pc_min, pc_max
 
 class PartNormalDataset(Dataset):
-    def __init__(self, data, npoints, use_conf_channel, use_utm_coords=True):
+    def __init__(self, data, npoints):
         self.data = data
         self.npoints = npoints
-        self.use_conf_channel = use_conf_channel
-                            #   use_utm_coords      use_conf_channel                          
-        self.column_indices = { True:           {   True: [1, 2, 5, 6], # use x,y,elev,signal_conf
-                                                    False: [1, 2, 5]},  # use x,y,elev
-                                False:          {   True: [3, 4, 5, 6], # use lon,lat,elev,signal_conf
-                                                    False: [3, 4, 5]}   # use lon,lat,elev
-                                }[use_utm_coords][use_conf_channel]
-
+        self.column_indices = [1, 2, 5, 6] # use x,y,elev,signal_conf
 
     def __getitem__(self, index):
         r0 = self.npoints * index
         r1 = r0 + self.npoints
         cls = np.array([0]).astype(np.int32)
         point_set = np.copy(self.data[r0:r1, self.column_indices])
-        if self.use_conf_channel:
-            point_set[:, -1] = point_set[:, -1].astype(np.int32)
+        point_set[:, -1] = point_set[:, -1].astype(np.int32) # retype the signal confidence column
         point_set_normalized = point_set
         point_set_normalized[:, 0:3], pc_min, pc_max = pc_normalize(point_set[:, 0:3])
         point_set_normalized_mask = np.full(self.npoints, True, dtype=bool)
-        # resample
+        # pad out the returned dataset to the fixed npoints
         length = len(point_set)
         if length < self.npoints:
             pad_point = np.ones((self.npoints-length, len(self.column_indices)), dtype=np.float64)
-            if self.use_conf_channel:
-                pad_point[:, -1] = pad_point[:, -1].astype(np.int32)
+            pad_point[:, -1] = pad_point[:, -1].astype(np.int32) # retype the signal confidence padded column
             point_set_normalized = np.concatenate((point_set_normalized, pad_point), axis=0)
             # create mask for point set - mask out the padded points
             pad_point_bool = np.full(self.npoints - length, False, dtype=bool)
             point_set_normalized_bool = np.full(length, True, dtype=bool)
             point_set_normalized_mask = np.concatenate((point_set_normalized_bool, pad_point_bool))
-
+        # return normalized dataset
         return point_set_normalized, cls, point_set_normalized_mask, pc_min, pc_max, (r0,r1)
 
     def __len__(self):
         return math.ceil(len(self.data) / self.npoints)
 
+################
+# RUN POINTNET2
+################
 
-os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 # set device
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-TEST_DATASET = PartNormalDataset(data, num_point, use_signal_conf)
-testDataLoader = torch.utils.data.DataLoader(TEST_DATASET, batch_size=batch_size, shuffle=False, num_workers=3)
+# create normalized dataset to process
+TEST_DATASET = PartNormalDataset(data, num_point)
+testDataLoader = torch.utils.data.DataLoader(TEST_DATASET, batch_size=1, shuffle=False, num_workers=3)
 print("The number of test data is: %d" % len(TEST_DATASET))
 num_classes = 1
 num_part = 2
 
-'''MODEL LOADING'''
+# load pointnet2 ML model
 sys.path.append('/pointnet2')
 sys.path.append('/pointnet2/models')
 MODEL = importlib.import_module('pointnet2_part_seg_msg')
-classifier = MODEL.get_model(num_part, conf_channel=use_signal_conf).to(device)
+classifier = MODEL.get_model(num_part, conf_channel=True).to(device)
 trained_model = torch.load('/pointnet2/trained_model/model.pth', map_location=torch.device(device))
-
 model_state_dict = {k.replace('module.', ''): v for k, v in trained_model['model_state_dict'].items()}
 classifier.load_state_dict(model_state_dict)
 
+# run classifier
 with torch.no_grad():
-
     classifier = classifier.eval()
     for batch_id, (points, label, point_set_normalized_mask, pc_min, pc_max, row_slice) in \
             tqdm(enumerate(testDataLoader), total=len(testDataLoader), smoothing=0.9):
-
         cur_batch_size, NUM_POINT, _ = points.size()
+        assert cur_batch_size == 1 # we've hardcoded the batch size to 1 above
         points, label = points.float().to(device), label.long().to(device)
         points = points.transpose(2, 1)
         vote_pool = torch.zeros(cur_batch_size, NUM_POINT, num_part).to(device)
@@ -280,25 +264,23 @@ with torch.no_grad():
 
         cur_pred = seg_pred.cpu().numpy()
         cur_pred_val = np.zeros((cur_batch_size, NUM_POINT)).astype(np.int32)
-        cur_pred_prob = np.zeros((cur_batch_size, NUM_POINT)).astype(np.float64)
         point_set_normalized_mask = point_set_normalized_mask.numpy()
 
         prob = np.exp(cur_pred[0, :, :])
-        cur_pred_prob[0, :] = prob[:, 1]  # the probability of belonging to seafloor class
         cur_pred_val[0, :] = np.where(prob[:, 1] < threshold, 0, 1)
         cur_mask = point_set_normalized_mask[0, :]
-        cur_pred_prob_mask = cur_pred_prob[0, cur_mask]
         cur_pred_val_mask = cur_pred_val[0, cur_mask]
 
-
+        # build dataframe of index and classification
         index_ph = data[row_slice[0]:row_slice[1], 0]
         class_ph = data[row_slice[0]:row_slice[1], 7]
         columns = {'index_ph': index_ph, 'class_ph': class_ph}            
         output_df = pd.DataFrame(columns)
         output_df.loc[output_df['class_ph'] == 3, 'class_ph'] = 1   # other
         output_df.loc[output_df['class_ph'] == 5, 'class_ph'] = 41  # sea surface
-        output_df.loc[cur_pred_val_mask == 1, 'class_ph'] = 40   # bathymetry
+        output_df.loc[cur_pred_val_mask == 1, 'class_ph'] = 40      # bathymetry
 
+        # convert dataframe to 2d numpy array for writing to csv
         output_data = output_df.to_numpy().astype(np.int32)
 
         # output file
