cee270ee7b3d8560ee28fa9191929a1f4d2bc692
JP Swinski <jp.swinski@nasa.gov>
2024-06-05 21:54:06 +0000

    coastnet builds again

--- plugins/icesat2/containers/oceaneyes/coastnet/bathy.sh
+++ plugins/icesat2/containers/oceaneyes/coastnet/bathy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 /coastnet/build/release/classify_coastnet --class=40 --aspect-ratio=4 --network-filename=/coastnet/models/coastnet-bathy.pt < $3 > $4
-source /venv/bin/activate
-python /coastnet/trimmer.py $4
+/env/bin/python /coastnet/trimmer.py $4
