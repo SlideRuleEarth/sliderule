ba2285ef268907273ae67792534f3d201e942987
JP Swinski <jp.swinski@nasa.gov>
2024-07-01 20:52:56 +0000

    updated coastnet integrated in

---
+++ plugins/icesat2/containers/oceaneyes/coastnet/runner.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+/coastnet/build/release/classify --verbose --model-filename=/coastnet/models/model-20240628.json < $3 > $4
+/env/bin/python /coastnet/trimmer.py $4
--- plugins/icesat2/containers/oceaneyes/coastnet/surface.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-/coastnet/build/release/classify_coastnet --verbose --class=41 --aspect-ratio=4 --network-filename=/coastnet/models/coastnet-surface.pt < $3 > $4
-/env/bin/python /coastnet/combiner.py $3 $4
