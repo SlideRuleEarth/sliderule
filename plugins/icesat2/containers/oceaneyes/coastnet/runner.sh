f2591b4aaf393b4fdbd28629da4ea5da9e08fb0d
JP Swinski <jp.swinski@nasa.gov>
2024-07-02 21:44:04 +0000

    fixed model path in coastnet runner

--- plugins/icesat2/containers/oceaneyes/coastnet/runner.sh
+++ plugins/icesat2/containers/oceaneyes/coastnet/runner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-/coastnet/build/release/classify --verbose --model-filename=/coastnet/models/model-20240628.json < $3 > $4
+/coastnet/build/release/classify --verbose --model-filename=/coastnet/models/coastnet_model-20240628.json < $3 > $4
 /env/bin/python /coastnet/trimmer.py $4
