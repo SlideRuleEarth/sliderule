453aa32eba00ac8d794553ea02264a2f704994a5
JP Swinski <jp.swinski@nasa.gov>
2024-08-07 17:21:55 +0000

    removed the oceaneyes container code to its own repo

--- datasets/icesat2/containers/oceaneyes/openoceans/runner.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-/openoceans/build/release/classify --verbose < $3 > $4
-/env/bin/python /openoceans/combiner.py $3 $4
--- datasets/icesat2/containers/oceaneyes/qtrees/runner.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-/qtrees/build/release/classify --verbose --model-filename=/qtrees/models/model-20240607.json < $3 > $4
-/env/bin/python /qtrees/combiner.py $3 $4
