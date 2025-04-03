f7c5f8f0598ab76c846c5dce4125631d51092902
JP Swinski <jp.swinski@nasa.gov>
2025-04-03 14:47:46 +0000

    refined in cloud check for azure which returns a 404

--- packages/aws/extensions/aws_utils.lua
+++ packages/aws/extensions/aws_utils.lua
@@ -1,12 +1,12 @@
 
 local function config_aws ()
     local status = false
-    local aws_rsps_code = core.servicecheck("http://169.254.169.254/latest/meta-data/", 1)
-    if aws_rsps_code >= 200 and aws_rsps_code < 500 then
-        print("Executing in the cloud: "..tostring(aws_rsps_code))
+    local http_code = core.servicecheck("http://169.254.169.254/latest/meta-data/", 1)
+    if (http_code >= 200 and http_code < 300) or (http_code == 401) then
+        print("Executing in the cloud: "..tostring(http_code))
         status = true
     else
-        print("Executing locally: "..tostring(aws_rsps_code))
+        print("Executing locally: "..tostring(http_code))
     end
     sys.setincloud(status)
     return status
