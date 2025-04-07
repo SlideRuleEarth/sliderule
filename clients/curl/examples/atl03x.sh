ab963285780c68f646a1e1940647827b7c88c3ec
JP Swinski <jp.swinski@nasa.gov>
2025-04-07 17:17:31 +0000

    at24x example for curl

--- clients/curl/examples/atl03x.sh
+++ clients/curl/examples/atl03x.sh
@@ -1,1 +1,1 @@
-curl -X POST -o /tmp/atl24x.parquet -d @atl24x.json https://sliderule.slideruleearth.io/arrow/atl24x
+curl -X POST -o /tmp/atl03x.parquet -d @atl03x.json https://sliderule.slideruleearth.io/arrow/atl03x
---
+++ clients/curl/examples/atl24x.sh
@@ -0,0 +1,2 @@
+#curl -X POST -o /tmp/atl24x.parquet -d @atl24x.json https://sliderule.slideruleearth.io/arrow/atl24x
+curl -X POST -o /tmp/atl24x.parquet -d @atl24x.json http://localhost/arrow/atl24x
