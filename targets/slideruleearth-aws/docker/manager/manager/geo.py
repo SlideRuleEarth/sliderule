d5291b13371e001ba33d5df607039720e1e5a251
JP Swinski <jp.swinski@nasa.gov>
2025-05-29 15:38:02 +0000

    reverted change to geo

--- targets/slideruleearth-aws/docker/manager/manager/geo.py
+++ targets/slideruleearth-aws/docker/manager/manager/geo.py
@@ -1,35 +1,23 @@
 import geoip2.database
-from flask import current_app
-
-####################
-# Initialization
-####################
-
-geo_country = None
-geo_city = None
-geo_asn = None
-
-####################
-# Functions
-####################
+from flask import (g, current_app)
 
 def get_geo():
-    global geo_country, geo_city, geo_asn
-    if type(geo_country) == type(None):
-        geo_country = geoip2.database.Reader(current_app.config['GEOLITE2_COUNTRY'])
-        geo_city = geoip2.database.Reader(current_app.config['GEOLITE2_CITY'])
-        geo_asn = geoip2.database.Reader(current_app.config['GEOLITE2_ASN'])
-    return geo_country, geo_city, geo_asn
+    if 'geo_country' not in g:
+        g.geo_country = geoip2.database.Reader(current_app.config['GEOLITE2_COUNTRY'])
+        g.geo_city = geoip2.database.Reader(current_app.config['GEOLITE2_CITY'])
+        g.geo_asn = geoip2.database.Reader(current_app.config['GEOLITE2_ASN'])
+    return g.geo_country, g.geo_city, g.geo_asn
 
 def close_geo(e=None):
-    global geo_country, geo_city, geo_asn
-    if type(geo_country) != type(None):
+    geo_country = g.pop('geo_country', None)
+    geo_city = g.pop('geo_city', None)
+    geo_asn = g.pop('geo_asn', None)
+    if geo_country is not None:
         geo_country.close()
-    if type(geo_city) != type(None):
+    if geo_city is not None:
         geo_city.close()
-    if type(geo_asn) != type(None):
+    if geo_asn is not None:
         geo_asn.close()
 
 def init_app(app):
     app.teardown_appcontext(close_geo)
-
