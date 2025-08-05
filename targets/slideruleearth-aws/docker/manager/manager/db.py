ca07bad49ae2fb6d52356fb3388871806795381e
Eric Lidwa <eric.lidwa@nasa.gov>
2025-08-05 21:20:09 +0000

    pytest

--- targets/slideruleearth-aws/docker/manager/manager/db.py
+++ targets/slideruleearth-aws/docker/manager/manager/db.py
@@ -67,7 +67,14 @@ def init_db_command():
 
 def init_app(app):
     db = duckdb.connect(app.config['DATABASE'])
-    db.execute("INSTALL spatial;")
+
+    # Use LOAD instead of INSTALL in CI
+    if os.getenv('DUCKDB_LOAD_ONLY') == '1':
+        db.execute("LOAD spatial;")
+    else:
+        db.execute("INSTALL spatial;")
+        db.execute("LOAD spatial;")
+
     db.close()
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
