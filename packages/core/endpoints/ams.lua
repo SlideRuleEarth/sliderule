0d2a725dad834a88648a052813a729b9a1f356b7
JP Swinski <jp.swinski@nasa.gov>
2025-11-18 20:50:11 +0000

    added atl24 into ams

--- packages/core/endpoints/ams.lua
+++ packages/core/endpoints/ams.lua
@@ -30,17 +30,39 @@ OUTPUT:     json string of AMS output
 
 local json = require("json")
 local parms = json.decode(arg[1])
-
 local response = nil
-if parms["refid"] then
-    response = core.ams("GET", string.format("atl13?refid=%d", parms["refid"]))
-elseif parms["name"] then
-    response = core.ams("GET", string.format("atl13?name=%s", string.gsub(parms["name"], " ", "%%20")))
-elseif parms["coord"]["lat"] or parms["coord"]["lon"] then
-    response = core.ams("GET", string.format("atl13?lon=%f&lat=%f", parms["coord"]["lon"], parms["coord"]["lat"]))
+
+if _rqst["arg"] == "atl13" then
+    if parms["refid"] then
+        response = core.ams("GET", string.format("atl13?refid=%d", parms["refid"]))
+    elseif parms["name"] then
+        response = core.ams("GET", string.format("atl13?name=%s", string.gsub(parms["name"], " ", "%%20")))
+    elseif parms["coord"]["lat"] or parms["coord"]["lon"] then
+        response = core.ams("GET", string.format("atl13?lon=%f&lat=%f", parms["coord"]["lon"], parms["coord"]["lat"]))
+    else
+        response = json.encode({error="must supply at least one query parameter"})
+    end
+elseif _rqst["arg"] == "atl24" then
+    if parms["granule"] then
+        response = core.ams("GET", string.format("atl24/granule/%s", parms["granule"]))
+    else
+        local first_parm = true
+        local request_str = ""
+        for _,v in ipairs({"t0", "t1", "season", "photons0", "photons1", "meandepth0", "meandepth1", "mindepth0", "mindepth1", "maxdepth0", "maxdepth1", "poly"}) do
+            if parms[v] then
+                request_str = request_str .. string.format("%s%s=%s", first_parm and "" or "&", v, parms[v])
+                first_parm = false
+            end
+        end
+        if #request_str > 0 then
+            response = core.ams("GET", string.format("atl24?%s", request_str))
+        else
+            response = json.encode({error="must supply at least one query parameter"})
+        end
+    end
+else
+    response = json.encode({error=string.format("unrecognized product: %s", _rqst["arg"])})
 end
 
-if response then
-    return response
-end
+return response
 
