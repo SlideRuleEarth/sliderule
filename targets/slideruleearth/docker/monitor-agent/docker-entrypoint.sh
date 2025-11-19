b261ebb1666e6863cdcb21688aa38bc2f24fa461
JP Swinski <jp.swinski@nasa.gov>
2025-11-19 18:55:18 +0000

    running monitor agent processes with s6

--- targets/slideruleearth/docker/monitor-agent/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/local/bin/promtail -config.file=/usr/local/etc/promtail.yml &
-/usr/local/bin/node_exporter --path.procfs=/host/proc --path.sysfs=/host/sys --path.rootfs=/rootfs &
-/usr/bin/bash
