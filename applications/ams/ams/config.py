e53ba1647aec256ce763a2de6ab89cec7d572141
JP Swinski <jp.swinski@nasa.gov>
2025-12-02 18:43:50 +0000

    fixed selftests; initial commit for 3dep ams

--- applications/ams/ams/config.py
+++ applications/ams/ams/config.py
@@ -1,4 +1,5 @@
 ATL13_MAPPINGS = '/data/ATL13/atl13_mappings.json'
 ATL13_MASK = '/data/ATL13/inland_water_body.db'
 ATL24_DB = '/data/ATL24/atl24r2.db'
+USGS3DEP_DB = '/data/3DEP/3dep.db'
 MAX_RESOURCES = 50000
