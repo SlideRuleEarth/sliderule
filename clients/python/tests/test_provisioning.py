690719b807a42d35026e4ebcab014237e3ab0931
JP Swinski <jp.swinski@nasa.gov>
2025-12-05 17:53:46 +0000

    removed provisioning system test that conflicts with cloudformation deployments

--- clients/python/tests/test_provisioning.py
+++ clients/python/tests/test_provisioning.py
@@ -16,14 +16,19 @@ class TestProvisioning:
         session._Session__buildauthheader(headers, force_refresh=True)
         assert len(headers['Authorization']) > 8
 
-    def test_num_nodes_update(self, domain, organization):
-        sliderule.set_url(domain)
-        status = sliderule.authenticate(organization)
-        assert status
-        result = sliderule.update_available_servers(2,20)
-        assert len(result) == 2
-        assert type(result[0]) == int
-        assert type(result[1]) == int
+    # TO BE REMOVED
+    # We cannot have a test that makes a capacity request
+    # because the provisioning system will then automatically
+    # schedule a teardown for that capacity even if the
+    # resources were provisioned by cloudformation.
+    #def test_num_nodes_update(self, domain, organization):
+    #    sliderule.set_url(domain)
+    #    status = sliderule.authenticate(organization)
+    #    assert status
+    #    result = sliderule.update_available_servers(2,20)
+    #    assert len(result) == 2
+    #    assert type(result[0]) == int
+    #    assert type(result[1]) == int
 
     def test_bad_org(self, domain):
         sliderule.set_url(domain)
