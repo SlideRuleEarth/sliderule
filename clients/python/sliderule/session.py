9695e3f43ff32adb0f3430125b77a5bc77404192
JP Swinski <jp.swinski@nasa.gov>
2026-03-12 19:14:02 +0000

    catch exceptions when trying to sign request

--- clients/python/sliderule/session.py
+++ clients/python/sliderule/session.py
@@ -713,18 +713,21 @@ class Session:
         '''
         cryptographically sign the request
         '''
-        from cryptography.hazmat.primitives.serialization import load_ssh_private_key
-        with open(os.path.join(Path.home(), self.private_key), "rb") as file:
-            private_key = load_ssh_private_key(file.read(), password=None)
-        path_b64 = base64.urlsafe_b64encode(path.encode()).decode()
-        timestamp = str(int(datetime.now(timezone.utc).timestamp()))
-        body_b64 = base64.urlsafe_b64encode(body.encode()).decode()
-        canonical_string = f"{path_b64}:{timestamp}:{body_b64}"
-        message_bytes = canonical_string.encode("utf-8")
-        signature = private_key.sign(message_bytes)
-        signature_b64 = base64.b64encode(signature).decode("ascii")
-        headers["x-sliderule-timestamp"] = str(timestamp)
-        headers["x-sliderule-signature"] = signature_b64
+        try:
+            from cryptography.hazmat.primitives.serialization import load_ssh_private_key
+            with open(os.path.join(Path.home(), self.private_key), "rb") as file:
+                private_key = load_ssh_private_key(file.read(), password=None)
+            path_b64 = base64.urlsafe_b64encode(path.encode()).decode()
+            timestamp = str(int(datetime.now(timezone.utc).timestamp()))
+            body_b64 = base64.urlsafe_b64encode(body.encode()).decode()
+            canonical_string = f"{path_b64}:{timestamp}:{body_b64}"
+            message_bytes = canonical_string.encode("utf-8")
+            signature = private_key.sign(message_bytes)
+            signature_b64 = base64.b64encode(signature).decode("ascii")
+            headers["x-sliderule-timestamp"] = str(timestamp)
+            headers["x-sliderule-signature"] = signature_b64
+        except Exception as e:
+            logger.warning(f"Failed to sign request: {e}")
 
 
     #
